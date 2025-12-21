4b2e6c3902fc471fbecf417f6aff535cdef3bcbe
Kaimestry <khanhan137@outlook.com>
2025-12-21 23:40:29 +0700

    add: scalability when adding items

--- src/_root/pages/Report.tsx
+++ src/_root/pages/Report.tsx
@@ -1,11 +1,17 @@
-import Test from "../../features/ReportPage/Test";
+import { REPORT } from "../../features/ReportPage/report.config";
 
 const Report = () => {
   return (
     <div className="flex flex-wrap gap-4">
-      <div className="report-item">
-        <Test></Test>
-      </div>
+      {REPORT.map((group) => (
+        <div key={group.id} className="report-item">
+          <h2 className="border text-center">{group.title}</h2>
+
+          {group.items.map((item) => (
+            <div key={item.id}>{item.content}</div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
