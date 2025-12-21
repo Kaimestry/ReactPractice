bc5d9c00fbd58642c9c670c3e77c285286d4661a
Kaimestry <khanhan137@outlook.com>
2025-12-21 23:06:33 +0700

    fix: report item scalability

--- src/_root/pages/Report.tsx
+++ src/_root/pages/Report.tsx
@@ -1,26 +1,11 @@
+import Test from "../../features/ReportPage/Test";
+
 const Report = () => {
   return (
     <div className="flex flex-wrap gap-4">
-      <p className="report-item">tesjhsadfhkasdfhklt1</p>
-      <p className="report-item">teks=fadfhka2</p>
-      <p className="report-item">tesaklt3</p>
-      <p className="report-item">tessdfhklt4</p>
-      <p className="report-item">tesjhsadfhkasdfhkl5</p>
-      <p className="report-item">tesjhsadfhkasdfhklt1</p>
-      <p className="report-item">teks=fadfhka2</p>
-      <p className="report-item">tesaklt3</p>
-      <p className="report-item">tessdfhklt4</p>
-      <p className="report-item">tesjhsadfhkasdfhkl5</p>
-      <p className="report-item">tesjhsadfhkasdfhklt1</p>
-      <p className="report-item">teks=fadfhka2</p>
-      <p className="report-item">tesaklt3</p>
-      <p className="report-item">tessdfhklt4</p>
-      <p className="report-item">tesjhsadfhkasdfhkl5</p>
-      <p className="report-item">tesjhsadfhkasdfhklt1</p>
-      <p className="report-item">teks=fadfhka2</p>
-      <p className="report-item">tesaklt3</p>
-      <p className="report-item">tessdfhklt4</p>
-      <p className="report-item">tesjhsadfhkasdfhkl5</p>
+      <div className="report-item">
+        <Test></Test>
+      </div>
     </div>
   );
 };
